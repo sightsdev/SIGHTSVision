75c2766031d5916ca6cab3dc42361d46a0f8c1dd
Anthony Gambale <ants.gambale@gmail.com>
2019-07-05 13:08:41 +1000

    trus me

--- qr/qr-read-orig.py
+++ qr/qr-read-orig.py
@@ -1,11 +1,17 @@
+
 import cv2
 from pyzbar.pyzbar import decode
 import imutils
 from imutils.video import VideoStream
 import os
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-vs', '--videosource', default='r', help='-vs r for robot, -vs w for webcam.')
+args = vars(ap.parse_args())
 
 # ip
-ip = "10.0.0.3"
+ip = "10.0.0.4"
 
 def getNumber(line, prop):
         numbers = []
@@ -31,18 +37,28 @@ def dataArrayToString(line, prop):
                 number_string += num
         return number_string
 
-def readCode(image):
-    # read from camera source
-    im = image
+# grab stream
+if args['videosource'] == 'w':
+    source = VideoStream(src=0).start()
+elif args['videosource'] == 'r':
+    source = VideoStream(src="http://"+ip+":8081").start()
+
+# save codes
+codes = {}
+
+while (True):
+    # read front camera
+    im = source.read()
 
     # convert to grayscale
     im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        
+            
     # increase contrast
     contrast_image = cv2.equalizeHist(im)
 
     # decode QR code
     decoded = decode(contrast_image)
+    print(decoded)
     decodedString = dataArrayToString(str(decoded), "data=")
         
     # if theres something in the output of decoded()   
@@ -53,57 +69,29 @@ def readCode(image):
         [x1, y1, x2, y2] = [x, y, x+w, y+h]
         region = contrast_image[y1:y2, x1:x2]
 
-        # return
-        return decodedString, region
-    else:
-        return None, None
-
-# Uncomment to use web stream from robot
-front_camera = VideoStream(src="http://"+ip+":8081").start()
-side_1_camera = VideoStream(src="http://"+ip+":8082").start() # left/right could be the other way round
-side_2_camera = VideoStream(src="http://"+ip+":8083").start()
-#vs = VideoStream(src=0).start()
-#times = [] # to tell how fast it was
-codes = {} # to be saved
-
-while (True):
-        # arrange data
-        images = [front_camera.read(), side_1_camera.read(), side_2_camera.read()]
-        decodedStrings, regions = [], []
-
-        # collect decoded strings and regions
-        for image in images:
-            decodedString, region = readCode(image)
-            decodedStrings.append(decodedString)
-            regions.append(readCode(region))
-
         # print to console, if there is not nothing (= if there is something)
-        if (decodedStrings != [[""],[""],[""]]):
-            print(decodedStrings)
+        if (decodedString != ""):
+            print(decodedString)
 
         # add the string and the region to the codes dict
-        for i in range(2):
-            codes[decodedSrings[i]] = regions[i]
+        codes[decodedString] = region
 
         # draw keypoints window
         #cv2.imshow("Keypoints", im)
         # draw histogram window
-        cv2.imshow("Front", images[0])
-        cv2.imshow("Side 1", images[1])
-        cv2.imshow("Side 2", images[2])
+    cv2.imshow("Camera", contrast_image)
+    # exit condition
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+            break
 
-        # exit condition
-        k = cv2.waitKey(5) & 0xFF
-        if k == 27:
-                break
-
-# chuck the codes in a file
-f = open("results.txt","w")
+# chuck the codes in a fil
+f = open("res/results.txt","w")
 
 for key in codes:
     f.write(key+"\n")
     filename = "".join(x for x in key if x.isalnum())
-    cv2.imwrite(filename+".png", codes[key])
+    cv2.imwrite("res/"+filename+".png", codes[key])
 
 # close f
 f.close()
