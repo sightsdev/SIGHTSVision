8bfdc19fe789ed1c48ba480efb4161a965ff0156
Jack Toomey <jb2me1@gmail.com>
2021-03-18 16:16:44 +1100

    tweaked

--- motion_detector.py
+++ motion_detector.py
@@ -50,7 +50,7 @@ while True:
     # compute the absolute difference between the current frame and
 	# first frame
 	frameDelta = cv2.absdiff(firstFrame, gray)
-	thresh = cv2.threshold(frameDelta, 100, 255, cv2.THRESH_BINARY)[1]
+	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 	
     # dilate the thresholded image to fill in holes, then find contours
 	# on thresholded image
