97e9eb395753a821429cacfa5b204d083a95f24f
Anthony Gambale <ants.gambale@gmail.com>
2021-06-25 13:07:32 +1000

    removed unused file

--- neuralnetworktest.py
+++
@@ -1,8 +0,0 @@
-# Neural networks are best for vision
-# Most time used for neural network is used for gaining data
-# Use out-of-sample data to train neural network as it has never seen that data before.
-# Do not use in-sample data to train neural network.
-
-import torch
-import torchvision
-from 
